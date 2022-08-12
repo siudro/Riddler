7bb77421f10993105b3a23bdab4146a1278e5a52
Sedra <83130573+siudro@users.noreply.github.com>
2022-08-12 12:32:27 +0300

    add the riddler character

--- riddler.pl
+++ riddler.pl
@@ -24,13 +24,13 @@ playG(A,B,C,D,E,F,G,H,I,J,K,L):-(
         _, '),nl,write('and remember, you only have one chance to answer!'),                               
                               nl,nl,  write('What Does A Liar Do When He is Dead?'),read(A),
     (   ( riddle1(A) ->
- (write('Correct!'), nl);(write('wrong,he lies still.'))) ,   
+ (write('Correct!'), nl);(write('No,he lies still.'))) ,   
 
 nl,write('You Are El Rata Alada'),
         nl,write('Hint: use dot instead of . '),
 read(B),
     ( riddle2(B) ->
- (write('Correct!'), nl);(write('wrong,wwwdotrataaladadotcom'))),    
+ (write('Correct!'), nl);(write('No. No, no, no! it is wwwdotrataaladadotcom'))),    
 
 
 nl,
@@ -38,7 +38,7 @@ write('It can be cruel, poetic, or blind.
        But when it’s denied, it’s violence you may find'),
 read(C),
 ( riddle3(C) ->
- (write('Correct!'), nl);(write('wrong,justice'))),
+ (write('Correct!'), nl);(write('NO! AHHHHH! This is not how this was supposed to go! its justice!'))),
     nl,
 
 write('If you are justice, 
@@ -53,7 +53,7 @@ write('Since your justice is so select,
        please tell us, which vermin you are paid to protect?'),
 read(E),
     ( riddle5(E) ->
- (write('Correct!'), nl);(write('wrong,the rat'))),
+ (write('Correct!'), nl);(write('Interesting. You're missing the big picture. it is the rat'))),
     nl,
 
 write('I grew up from a seed, as tough as a weed.
@@ -61,13 +61,13 @@ write('I grew up from a seed, as tough as a weed.
        I will never know where I come from. Do you know what I am?'),
 read(F),
     ( riddle6(F) ->
- (write('Correct!'), nl);(write('wrong, an orphan'))),
+ (write('Correct!'), nl);(write('No! an orphan'))),
     nl,
 
 write('See you in hell.'),
 read(G),
    ( riddle7(G) ->
- (write('Correct!'), nl);(write('wrong, arkham asylum'))),
+ (write('Correct!'), nl);(write('NO! AHHHHH! arkham asylum'))),
     nl,
 
 
@@ -81,7 +81,7 @@ write('Riddle me this: The less of them you have,
        the more one is worth?'),
    read(I),
  ( riddle9(I) ->
- (write('Correct!'), nl);(write('wrong,a friend'))),
+ (write('Correct!'), nl);(write('You mean you didn't figure it out? It's a friend!'))),
     nl,
 
 write('What was new, is new again. Rebirth.
@@ -89,7 +89,7 @@ write('What was new, is new again. Rebirth.
 
     read(J),
 ( riddle10(J) ->
- (write('Correct!'), nl);(write('wrong, renewal'))),
+ (write('Correct!'), nl);(write('Oh, you're really not as smart as I thought you were. I guess I gave you too much credit. It's a renewal'))),
     nl,
 
 write('Fear he who hides behind one.'),
@@ -104,7 +104,7 @@ write('I am first a fraud or a trick.
        That’s up to your misinterpretation.'),
    read(L),
     ( riddle12(L) ->
- (write('Correct!'), nl,!);(write('wrong, confusion')))),
+ (write('yes!'), nl,!);(write('Do I need to show you more for you to understand? it's confusion')))),
     nl, write('data source: https://screenrant.com/the-batman-movie-riddler-riddle-meanings-explained/')).
    
    
